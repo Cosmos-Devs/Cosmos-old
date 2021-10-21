16fb8173858de4bef38a622f6e92f9275caafb55
Jochem van der Valk <jochemvandervalk@gmail.com>
2021-10-22 01:37:49 +0200

    created nav bar

---
+++ src/components/header.svelte
@@ -0,0 +1,20 @@
+<script>
+  import json from "../data/navs/mainmenu.json";
+  const items = json.items.map(({ title, slug }) => ({
+    title,
+    url: slug === "index" ? "/" : `/${slug}/`,
+  }));
+</script>
+
+<header class="flex flex-row">
+  <h2>Cosmos</h2>
+  <nav>
+    <ul>
+      {#each items as { url, title }}
+        <li>
+          <a href={url}>{title}</a>
+        </li>
+      {/each}
+    </ul>
+  </nav>
+</header>
