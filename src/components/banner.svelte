2bf8d35f6a8d4ec57593b56ae1102f8f322734f0
Jochem van der Valk <jochemvandervalk@gmail.com>
2021-10-23 01:17:50 +0200

    hide element if doesnt exists

--- src/components/banner.svelte
+++ src/components/banner.svelte
@@ -1,17 +1,21 @@
 <script>
   import marked from 'marked';
-  export let title = '';
-  export let content = '';
+  export let title;
+  export let content;
   export let image;
-  console.log(image);
 </script>
 
 <section class="bg-blue-100 px-5 py-10">
   <div class="container mx-auto grid grid-cols-1 md:grid-cols-2 gap-5">
-    <h1 class="text-3xl font-bold self-end">{title}</h1>
-    <div class="col-start-1">
-      {@html marked(content)}
-    </div>
+    {#if title}
+      <h1 class="text-3xl font-bold self-end">{title}</h1>
+    {/if}
+
+    {#if content}
+      <div class="col-start-1">
+        {@html marked(content)}
+      </div>
+    {/if}
 
     {#if image}
       <img class="md:col-start-2 row-start-1 md:row-end-3" src={image} alt="" />
