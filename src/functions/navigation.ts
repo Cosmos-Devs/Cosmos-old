482f92a8c16b2081efa09d7f536513360ab24f32
Jochem van der Valk <jochemvandervalk@gmail.com>
2021-10-22 15:29:37 +0200

    created a get nav items function

---
+++ src/functions/navigation.ts
@@ -0,0 +1,20 @@
+import json from "../data/navs/mainmenu.json"
+
+const createUrl = (slug) =>  slug === "index" ? "/" : `/${slug}/`;
+
+const getNavItems = () => {
+    const {items} = json;
+
+    if(!items || !Array.isArray(items)) {
+        throw new Error('"Items" is missing or is not an array');
+    }   
+
+    return items.map(({ type, title, slug, url, new_tab }) => (
+        {
+            title,
+            url: type === 'page' ? createUrl(slug) : url,
+            target: new_tab ? "_blank" : '_self'
+        }
+    ));
+}
+export const items = getNavItems();
