e51a3b2b16d8b492c3efbcf61695427489c2eca8
Jochem van der Valk <jochemvandervalk@gmail.com>
2021-10-22 15:29:51 +0200

    rewrite nav component

--- src/components/header.svelte
+++ src/components/header.svelte
@@ -1,18 +1,15 @@
 <script>
-  import json from "../data/navs/mainmenu.json";
-  const items = json.items.map(({ title, slug }) => ({
-    title,
-    url: slug === "index" ? "/" : `/${slug}/`,
-  }));
+  import { items } from '../functions/navigation.ts';
+  console.log('nav', items);
 </script>
 
 <header class="flex flex-row justify-between">
   <h2>Cosmos</h2>
   <nav>
     <ul class="flex flex-row">
-      {#each items as { url, title }}
+      {#each items as { url, title, target }}
         <li class="mr-3">
-          <a href={url}>{title}</a>
+          <a href={url} {target}>{title}</a>
         </li>
       {/each}
     </ul>
