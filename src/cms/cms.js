04fc419fd592e621bd6bc38e5ebfc1936b929a04
Jochem van der Valk <jochemvandervalk@gmail.com>
2021-10-23 20:21:33 +0200

    format

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,23 +1,23 @@
 import CMS from 'netlify-cms-app';
-import initPreviews from "./previews"
+import initPreviews from './previews';
 import Page from '../templates/page.svelte';
 
 const Init = () => {
-    CMS.init();
-   
-    // init previews
-    initPreviews(
-      [
-        {
-          name: 'pages',
-          component: Page,
-        },
-      ],
-      CMS
-    );
+  CMS.init();
 
-    // init styles
-    const links = document.querySelectorAll('[rel="stylesheet"]');
-    links.forEach((link) => CMS.registerPreviewStyle(link.href));
-}
-export default Init()
+  // init previews
+  initPreviews(
+    [
+      {
+        name: 'pages',
+        component: Page,
+      },
+    ],
+    CMS
+  );
+
+  // init styles
+  const links = document.querySelectorAll('[rel="stylesheet"]');
+  links.forEach((link) => CMS.registerPreviewStyle(link.href));
+};
+export default Init();
--- src/cms/previews.ts
+++ src/cms/previews.ts
@@ -1,20 +1,18 @@
-import toReact from "svelte-adapter/react";
-  import { SvelteComponent } from "svelte";
+import toReact from 'svelte-adapter/react';
+import { SvelteComponent } from 'svelte';
 
 interface PreviewInterface {
-    name: string,
-    component: new (arg: any) => SvelteComponent
+  name: string;
+  component: new (arg: any) => SvelteComponent;
 }
 
-const initPreviews = (items: PreviewInterface[], CMS) => 
-    items.forEach(({ name, component }) => {
-        
-        const ReactElement = toReact(component, {}, "div");
+const initPreviews = (items: PreviewInterface[], CMS) =>
+  items.forEach(({ name, component }) => {
+    const ReactElement = toReact(component, {}, 'div');
 
-        CMS.registerPreviewTemplate(name, (prvObj) => {
-            const content = prvObj?.entry?.getIn(['data'])?.toJS()
-            return ReactElement({ content });
-        })
-
-    })
-export default initPreviews
+    CMS.registerPreviewTemplate(name, (prvObj) => {
+      const content = prvObj?.entry?.getIn(['data'])?.toJS();
+      return ReactElement({ content });
+    });
+  });
+export default initPreviews;
