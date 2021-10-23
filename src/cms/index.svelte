98b1816fcfe4760d8b121bc256980b8a557dd0ab
Jochem van der Valk <jochemvandervalk@gmail.com>
2021-10-23 02:18:59 +0200

    setup cms ts

--- src/cms/index.svelte
+++ src/cms/index.svelte
@@ -1,13 +1,19 @@
+<script>
+  import { onMount } from 'svelte';
+
+  onMount(() => {
+    import('./cms.ts');
+  });
+</script>
+
 <html lang="en">
   <head>
     <meta charset="utf-8" />
     <link rel="icon" type="image/x-icon" href="/favicon.ico" />
     <meta name="viewport" content="width=device-width" />
     <title>Admin - Cosmos</title>
+    <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
   </head>
 
-  <body>
-    <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
-    <script src="https://unpkg.com/netlify-cms@^2.0.0/dist/netlify-cms.js"></script>
-  </body>
+  <body />
 </html>
---
+++ src/cms/previews/page.ts
@@ -0,0 +1,8 @@
+// import toReact from "svelte-adapter/react";
+// import Template from "../../templates/default.svelte";
+
+// const ReactElement = toReact(Template, {}, "div");
+// const PagesPreview = (cms) => {
+//     return ReactElement({ cms });
+// }
+// export default PagesPreview
