56332e4a1b250747dbac9e55f4de6b628d1496f7
Jochem van der Valk <jochemvandervalk@gmail.com>
2021-10-23 18:32:49 +0200

    register live preview

--- src/cms/previews/page.ts
+++
@@ -1,8 +0,0 @@
-// import toReact from "svelte-adapter/react";
-// import Template from "../../templates/default.svelte";
-
-// const ReactElement = toReact(Template, {}, "div");
-// const PagesPreview = (cms) => {
-//     return ReactElement({ cms });
-// }
-// export default PagesPreview
