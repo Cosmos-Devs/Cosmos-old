934e8b9fef592f4295dcc38f4dd7d9a101c06d21
Jochem van der Valk <jochemvandervalk@gmail.com>
2021-10-30 22:51:59 +0200

    ✨custom content component

---
+++ src/components/builder.svelte
@@ -0,0 +1,10 @@
+<script>
+  import blocks from '../cms/blocks';
+  export let content = [];
+</script>
+
+{#each content as { block, ...props }}
+  {#if blocks[block]}
+    <svelte:component this={blocks[block].view} {...props} />
+  {/if}
+{/each}
