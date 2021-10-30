f484a39ed873915c5a9b7e422a67a10d0f9aa623
Jochem van der Valk <jochemvandervalk@gmail.com>
2021-10-30 22:51:26 +0200

    ✨blocks init file

---
+++ src/cms/blocks.ts
@@ -0,0 +1,9 @@
+import BannerView from "../components/blocks/banner/index.svelte"
+import BannerControl from "../components/blocks/banner/control.svelte"
+
+export default {
+    banner: {
+        view: BannerView,
+        control: BannerControl
+    }
+}
--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,9 +1,21 @@
 import CMS from 'netlify-cms-app';
-import initPreviews from './previews';
+import initPreviews from '../lib/previews';
 import Page from '../templates/page.svelte';
+import blocks from "./blocks";
 
 export default function (styling) {
   CMS.init();
+
+
+  console.log('blocks',blocks)
+  // CMS.registerWidget(
+  //   'test',
+  //   (e) => console.log('control',e));
+
+  // initWidgets(
+  //   [Banner],
+  //   CMS
+  // );
 
   initPreviews(
     [
