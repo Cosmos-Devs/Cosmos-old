ebbd3897a55f60a891904590e2560c6699b3c35b
Jochem van der Valk <jochemvandervalk@gmail.com>
2021-10-31 13:53:24 +0100

    ✨ add netilfy widget component

---
+++ src/cms/widget.svelte
@@ -0,0 +1,15 @@
+<script>
+  import ReactComponent from 'svelte-react';
+  import { getContext } from 'svelte';
+
+  export let type;
+  export let name;
+  export let value;
+  export let onChange;
+
+  const CMS = getContext('CMS');
+  const editor = getContext('editor');
+  const NetilfyWidget = CMS.getWidget(type).control;
+</script>
+
+<ReactComponent this={NetilfyWidget} {...editor} onChange={(val) => onChange(val, name)} {value} />
