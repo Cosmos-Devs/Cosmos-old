2d543a73efc58b60eb52598e4dea3ca8c6f9d1ba
Niels Mentink <niels@ment.ink>
2021-10-31 18:51:10 +0100

    💄 Updated header styles

--- src/components/header.svelte
+++ src/components/header.svelte
@@ -1,16 +1,20 @@
 <script>
   import { items } from '$/lib/navigation.ts';
+  import Logo from './Logo.svelte';
 </script>
 
-<header class="flex flex-row justify-between">
-  <h2>Cosmos</h2>
-  <nav>
-    <ul class="flex flex-row">
-      {#each items as { url, title, target }}
-        <li class="mr-3">
-          <a href={url} {target}>{title}</a>
-        </li>
-      {/each}
-    </ul>
-  </nav>
+<header class="flex bg-indigo-700 text-white">
+  <div class="container justify-between align-center flex">
+    <Logo />
+
+    <nav>
+      <ul class="flex flex-row">
+        {#each items as { url, title, target }}
+          <li class="mr-3">
+            <a href={url} {target}>{title}</a>
+          </li>
+        {/each}
+      </ul>
+    </nav>
+  </div>
 </header>
