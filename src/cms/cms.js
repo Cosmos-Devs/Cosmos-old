1e1b5755695a82c9a0597285a328ed106a70a6ac
Jochem van der Valk <jochemvandervalk@gmail.com>
2021-10-31 13:54:03 +0100

    update blocks init

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,21 +1,12 @@
 import CMS from 'netlify-cms-app';
 import initPreviews from '../lib/previews';
+import initBlocks from '../lib/blocks';
 import Page from '../templates/page.svelte';
-import blocks from "./blocks";
 
 export default function (styling) {
   CMS.init();
 
-
-  console.log('blocks',blocks)
-  // CMS.registerWidget(
-  //   'test',
-  //   (e) => console.log('control',e));
-
-  // initWidgets(
-  //   [Banner],
-  //   CMS
-  // );
+  initBlocks(CMS)
 
   initPreviews(
     [
