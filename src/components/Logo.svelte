b30f3ef40449c362956b557861c178cbada7b1e7
Niels Mentink <niels@ment.ink>
2021-10-31 18:48:47 +0100

    ✨ Added logo component

---
+++ src/components/Logo.svelte
@@ -0,0 +1,10 @@
+<script>
+  import Icon from './Icon.svelte';
+
+  export let href = '/';
+</script>
+
+<a {href} class="grid grid-flow-col items-center gap-2">
+  <Icon size="48" />
+  <h2>Cosmos</h2>
+</a>
