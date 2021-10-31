092d9c1cc37c0c99f1bec7c69f270c283c0c7c6c
Niels Mentink <niels@ment.ink>
2021-10-31 18:53:41 +0100

    🔧 Updated tailwind config to center containers

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,9 @@ module.exports = {
   purge: ['./public/**/*.html', './src/**/*.{astro,js,jsx,svelte,ts,tsx,vue}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    container: {
+      center: true,
+    },
   },
   variants: {
     extend: {},
