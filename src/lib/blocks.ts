ee8d3cb0c1a24c248164a2c2acbf10710e019c2f
Jochem van der Valk <jochemvandervalk@gmail.com>
2021-10-31 13:52:57 +0100

    ✨create context wrapper

--- src/lib/blocks.ts
+++ src/lib/blocks.ts
@@ -1,15 +1,18 @@
 import toReact from 'svelte-adapter/react';
-import blocks from "./blocks";
+import blocks from "../cms/blocks";
+import Wrapper from "./cmsWrapper.svelte"
+import { SvelteComponent } from 'svelte';
+
 
 export default function (CMS) {
     Object.keys(blocks).forEach(name => {
         const { control } = blocks[name];
-        const ReactElement = toReact(control, {}, 'div');
+        const ReactElement = toReact(Wrapper as any, {}, 'div');
 
         CMS.registerWidget(
             name,
             (editor) => {
-                return ReactElement({ CMS, editor })
+                return ReactElement({ CMS, editor, component: control })
             }
         );
     })
