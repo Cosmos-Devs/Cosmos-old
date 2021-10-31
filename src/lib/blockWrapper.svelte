2ff129ce285b508a692fb35bd2828ff88baacf34
Jochem van der Valk <jochemvandervalk@gmail.com>
2021-10-31 16:07:17 +0100

    🎨 outside data functions

---
+++ src/lib/blockWrapper.svelte
@@ -0,0 +1,25 @@
+<script>
+  import { setContext } from 'svelte';
+  import { writable } from 'svelte/store';
+  import { fromJS } from 'immutable';
+
+  export let CMS;
+  export let editor;
+  export let component;
+
+  setContext('CMS', CMS);
+  setContext('editor', editor);
+
+  const value = writable(editor?.value?.toJS() || '');
+
+  value.subscribe((value) => {
+    const data = fromJS(value);
+    editor.onChange(data);
+  });
+</script>
+
+<div>
+  {#if component}
+    <svelte:component this={component} {value} {CMS} {editor} />
+  {/if}
+</div>
